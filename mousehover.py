ac5095a6c86a06c90c7ec0657b51c85dcb9398fb
morteza <m67.heidari@gmail.com>
2022-03-04 17:09:29 -0500

    remove mousehover.py

--- mousehover.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import time
-while True:
-    # pyautogui.click(100, 100)
-    pyautogui.moveTo(100, 150)
-    pyautogui.moveRel(0, 10)  # move mouse 10 pixels down
-    pyautogui.dragTo(100, 150)
-    pyautogui.dragRel(0, 10)  # drag mouse 10 pixels down
-    time.sleep(10)
